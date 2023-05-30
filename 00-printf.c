8f54638bee5eded3d8becb9b321e8bf3842c9e8f
Sylviaani <sylviaani26@gmail.com>
2023-05-29 18:13:21 -0700

    commit message

--- 00-printf.c
+++ 00-printf.c
@@ -16,11 +16,11 @@ int _printf(const char *format, ...)
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
-		return (-1);
+	return (-1);
 
 	va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0', i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
@@ -35,11 +35,11 @@ int _printf(const char *format, ...)
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, liat);
+			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
@@ -63,7 +63,6 @@ void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
-
 
 	*buff_ind = 0;
 }
